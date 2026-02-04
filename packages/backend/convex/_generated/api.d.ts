fb66df3d42afc0a6572a57dc1e1ebb4b666e36b2
Jan Härkönen <jan.harkonen@gmail.com>
2026-02-05 00:01:27 +0200

    remove convex schemas and unused functions, update autogen files

--- packages/backend/convex/_generated/api.d.ts
+++ packages/backend/convex/_generated/api.d.ts
@@ -12,7 +12,6 @@ import type * as auth from "../auth.js";
 import type * as healthCheck from "../healthCheck.js";
 import type * as http from "../http.js";
 import type * as privateData from "../privateData.js";
-import type * as todos from "../todos.js";
 
 import type {
   ApiFromModules,
@@ -25,7 +24,6 @@ declare const fullApi: ApiFromModules<{
   healthCheck: typeof healthCheck;
   http: typeof http;
   privateData: typeof privateData;
-  todos: typeof todos;
 }>;
 
 /**
