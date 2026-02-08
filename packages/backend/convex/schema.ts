8c2121a05dc4fd81cbc6cb95ef8c8b57e2ca190b
Jan Härkönen <jan.harkonen@gmail.com>
2026-02-08 17:53:06 +0200

    update schema

--- packages/backend/convex/schema.ts
+++ packages/backend/convex/schema.ts
@@ -3,14 +3,15 @@ import { v } from "convex/values";
 
 export default defineSchema({
   receipts: defineTable({
-    alv: v.float64(),
+    workspace_id: v.id("workspaces"),
     category: v.string(),
-    file_id: v.string(),
+    name: v.string(),
     price: v.float64(),
-    workspace_id: v.string(),
+    alv: v.float64(),
+    file_id: v.string(),
   }),
   workspaces: defineTable({
     access_rights: v.array(v.string()),
     workspace_name: v.string(),
-  }).index("by_access_rights", ["access_rights"]),
+  }),
 });
